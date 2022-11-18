371f49cf190aa1cefe8c211a5bf0e2cf147aeeab
Nicolas LHOMME <nicolas.lhomme@rca.fr>
2022-11-18 20:00:56 +0100

    minestrator-backuper.sh: updating variables

--- files/minestrator-backuper.sh
+++ files/minestrator-backuper.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-# VARIABLES USED
-#minestratorUsername=
-#minestratorPassword= 
-#minestratorServer=
+# VARIABLES
+#sftpUsername=
+#sftpPassword=
+#sftpServer=
 #transfertSpeedLimit=
 
 
 # START
 
-# Retrieving the Minecraft server files from Minestrator via SSHPASS the Minecraft server files from Minestrator via SSHPASS (password autofill) + SFTP to the input folder
-/usr/bin/sshpass -p "$minestratorPassword" /usr/bin/sftp -oHostKeyAlgorithms=+ssh-rsa -oStrictHostKeyChecking=accept-new -P 2022 -r -p -l "$transfertSpeedLimit" "$minestratorUsername"@"$minestratorServer":/ /input/
+# Retrieving the Minecraft server files from Minestrator via SSHPASS the Minecraft server files from sftp via SSHPASS (password autofill) + SFTP to the input folder
+/usr/bin/sshpass -p "$sftpPassword" /usr/bin/sftp -oHostKeyAlgorithms=+ssh-rsa -oStrictHostKeyChecking=accept-new -P 2022 -r -p -l "$transfertSpeedLimit" "$sftpUsername"@"$sftpServer":/ /input/
 
 # Compressing the Minecraft files to a tar.gz archive
-/usr/bin/tar zcfv output/"$(date +\%Y\%m\%d)""-minecraft.tar.gz" -C input .
+/usr/bin/tar zcfv output/"$(date +\%Y\%m\%d)""-minestrator.tar.gz" -C input .
 
 # END
